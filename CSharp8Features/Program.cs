5d050f63242660fde59d27182e707d9f8c0b02e0
Vishnu Udayakumar <Vishnu_Udayakumar@ajg.com>
2019-12-04 17:49:52 +0000

    ReadOnlyMember loop added

--- CSharp8Features/Program.cs
+++ CSharp8Features/Program.cs
@@ -6,6 +6,10 @@ namespace CSharp8Features
     {
         static void Main(string[] args)
         {
+            for (int i = 0; i < 20; i++)
+            {
+                
+            }
             ReadOnlyMember.ReadOnlyMember rd = new ReadOnlyMember.ReadOnlyMember();
             rd.X = 10;
             rd.Y = 20; 
--- CSharp8Features/ReadOnlyMember/ReadOnlyMemeber.cs
+++ CSharp8Features/ReadOnlyMember/ReadOnlyMemeber.cs
@@ -8,7 +8,7 @@ namespace CSharp8Features.ReadOnlyMember
     {
         public double X { get; set; }
         public double Y { get; set; }
-        public double Distance => Math.Sqrt(X * X + Y * Y);
+        public readonly double Distance => Math.Sqrt(X * X + Y * Y);
 
         public override readonly  string ToString() =>
             $"({X}, {Y}) is {Distance} from the origin";
