35397adda63d573d3575ff8f575ee5f008ff755e
Vishnu Kumar <microsophtdeveloper@outlook.com>
2019-12-07 22:42:03 +0000

    ReadOnlyMember - changed to radius

--- CSharp8Features/Program.cs
+++ CSharp8Features/Program.cs
@@ -7,44 +7,44 @@ namespace CSharp8Features
     {
         static void Main(string[] args)
         {
-            CallReadOnlyMember();
+            //CallReadOnlyMember();
             Console.ReadLine();
         }
 
-        static void CallReadOnlyMember()
-        {
-            Console.WriteLine("Do you want to run test");
-            string value = Console.ReadLine();
+        //static void CallReadOnlyMember()
+        //{
+        //    Console.WriteLine("Do you want to run test");
+        //    string value = Console.ReadLine();
 
-            Stopwatch stopWatch = new Stopwatch();
-            if (value != null && value.ToLower().Equals("yes"))
-            {
-                long kbAtExecution = GC.GetTotalMemory(false) / 1024;
-                stopWatch.Start();
-                for (Int64 i = 0; i < 2000000; i++)
-                {
-                    ReadOnlyMember.ReadOnlyMember rd = new ReadOnlyMember.ReadOnlyMember();
-                    rd.X = 10;
-                    rd.Y = 20;
+        //    Stopwatch stopWatch = new Stopwatch();
+        //    if (value != null && value.ToLower().Equals("yes"))
+        //    {
+        //        long kbAtExecution = GC.GetTotalMemory(false) / 1024;
+        //        stopWatch.Start();
+        //        for (Int64 i = 0; i < 2000000; i++)
+        //        {
+        //            ReadOnlyMember.ReadOnlyMember rd = new ReadOnlyMember.ReadOnlyMember();
+        //            rd.X = 10;
+        //            rd.Y = 20;
 
-                    Console.WriteLine($"Count{i}:{rd.ToString()}");
-                }
+        //            Console.WriteLine($"Count{i}:{rd.ToString()}");
+        //        }
 
-                stopWatch.Stop();
-                long kbAfter1 = GC.GetTotalMemory(false) / 1024;
-                //long kbAfter2 = GC.GetTotalMemory(true) / 1024;
-                TimeSpan ts = stopWatch.Elapsed;
-                string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
-                    ts.Hours, ts.Minutes, ts.Seconds,
-                    ts.Milliseconds / 10);
-                Console.WriteLine($"Process ended");
-                Console.WriteLine(kbAtExecution + " Started with this kb.");
-                Console.WriteLine(kbAfter1 + " After the test.");
-                Console.WriteLine(kbAfter1 - kbAtExecution + " Total Collected by GC");
-                Console.WriteLine($"Time Elapsed {elapsedTime}");
-                //Console.WriteLine(kbAfter2 + " Amt. After Collection");
-                //Console.WriteLine(kbAfter2 - kbAfter1 + " Amt. Collected by GC.");
-            }
-        }
+        //        stopWatch.Stop();
+        //        long kbAfter1 = GC.GetTotalMemory(false) / 1024;
+        //        //long kbAfter2 = GC.GetTotalMemory(true) / 1024;
+        //        TimeSpan ts = stopWatch.Elapsed;
+        //        string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+        //            ts.Hours, ts.Minutes, ts.Seconds,
+        //            ts.Milliseconds / 10);
+        //        Console.WriteLine($"Process ended");
+        //        Console.WriteLine(kbAtExecution + " Started with this kb.");
+        //        Console.WriteLine(kbAfter1 + " After the test.");
+        //        Console.WriteLine(kbAfter1 - kbAtExecution + " Total Collected by GC");
+        //        Console.WriteLine($"Time Elapsed {elapsedTime}");
+        //        //Console.WriteLine(kbAfter2 + " Amt. After Collection");
+        //        //Console.WriteLine(kbAfter2 - kbAfter1 + " Amt. Collected by GC.");
+        //    }
+        //}
     }
 }
--- CSharp8Features/ReadOnlyMember/ReadOnlyMemeber.cs
+++ CSharp8Features/ReadOnlyMember/ReadOnlyMemeber.cs
@@ -1,17 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace CSharp8Features.ReadOnlyMember
 {
     struct ReadOnlyMember
     {
-        public double X { get; set; }
-        public double Y { get; set; }
-        public readonly double Distance => Math.Sqrt(X * X + Y * Y);
+        public double Radius { get; set; }
+       
+        public readonly double Circumference => 2 * Math.PI * Radius;
 
-        public override readonly string ToString() =>
-            $"({X}, {Y}) is {Distance} from the origin";
-    } 
-    
+        public readonly override string ToString()
+        {
+            return $"Circumference of the circle is {Circumference}"; 
+        }
+    }
+
 }
