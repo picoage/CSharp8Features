f0b746772ecb7f52d7028375203dd09b34db7846
Vishnu Kumar <microsophtdeveloper@outlook.com>
2020-04-19 09:34:17 +0100

    default memeber

---
+++ CSharp8Features/UserDeclaration/FileGenarator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CSharp8Features.UserDeclaration
+{
+    internal class FileGenarator
+    {
+        internal bool WriteToFile(List<string> values)
+        {
+            using var file = new StreamWriter("NewFile.txt");
+            
+                if (!values.Any()) return false;
+
+                foreach (var value in values)
+                {
+                    file.Write(value);
+                }
+                Console.WriteLine("FileWriten");
+            
+
+            return true;
+        }
+    }
+}
